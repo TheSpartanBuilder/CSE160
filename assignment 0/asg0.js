0e485657c01a0937e0614e5e0d12dcdb14a393d1
Thomas Wong <147549325+TheSpartanBuilder@users.noreply.github.com>
2025-01-07 16:34:10 -0800

    Did part 3

--- assignment 0/asg0.js
+++ assignment 0/asg0.js
@@ -16,6 +16,7 @@ function main() {
 
     function drawVector(v,color)
     {
+        //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/lineTo
         ctx.strokeStyle = color;
         ctx.beginPath();
         ctx.moveTo(200,200);
@@ -24,6 +25,28 @@ function main() {
     }
 
     // Trying the draw the line using Vector 3
-    var v1 = new Vector3([2.5,2.5,0]);
-    drawVector(v1,'red');
+    // This is step 2 or part 2
+    // var v1 = new Vector3([2.5,2.5,0]);
+    // drawVector(v1,'red');
+
+
+    // Trying to implement the button
+    let v1_button = document.getElementById("v1_button");
+    v1_button.addEventListener( "click" ,handleDrawEvent);
+    //v1_button.onclick(doDraw);
+
+    function handleDrawEvent() {
+        // Clear the canvas
+        ctx.fillStyle = 'rgba(0, 0, 0, 1.0)'; // Set a black color
+        ctx.fillRect(0, 0, 400, 400); // Fill a rectangle with the color
+        // console.log("run");
+        const v1_x = document.getElementById("v1_x");
+        const v1_y = document.getElementById("v1_y");
+        // console.log(v1_x.value);
+        // console.log(v1_y.value);
+        let v1 = new Vector3([v1_x.value,v1_y.value,0]);
+        drawVector(v1,'red');
+        // https://stackoverflow.com/questions/71569705/why-eventlistener-reloads-the-page-and-onclick-does-not
+        // event.preventDefault();
+    }
 } 
